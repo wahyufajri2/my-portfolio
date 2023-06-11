bf626bae64a1ef3fea9762ea8788fab6df6e754c
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-06-11 11:51:56 +0700

    Menambahkan toggle icon navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+/*===== toggle icon navbar=====*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 /*===== scroll section active link=====*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -16,4 +25,8 @@ window.onscroll = () => {
             });
         };
     });
+    /*===== sticky navbar =====*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
 };
