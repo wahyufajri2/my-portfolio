2ef9ac1e1af6371e7cdb8e1a7c88cbd346fa06ff
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-06-11 12:59:37 +0700

    Menambahkan scroll reveal

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,4 +29,21 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*===== remove toggle icon and navbar when click navbar link (scroll) =====*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+/*===== scroll reveal =====*/
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
