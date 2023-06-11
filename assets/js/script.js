165ae73ed235e99d6bcb15e2353f511d3f56f15c
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2023-06-11 13:17:28 +0700

    Menambahkan typed menggunakan js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,7 @@ window.onscroll = () => {
 
 /*===== scroll reveal =====*/
 ScrollReveal({
-    // reset: true,
+    reset: true,
     distance: '80px',
     duration: 2000,
     delay: 200
@@ -47,3 +47,12 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/*===== typed js =====*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'UI/UX Designer', 'Youtuber', 'Freelancer', 'Photographer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
