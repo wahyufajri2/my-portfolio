1babd07b52b4178e9656dba0057a3cebc6ca486d
wahyufajri2 <2011501020@student.unisayogya.ac.id>
2024-10-28 20:05:16 +0700

    fix: memperbarui tampilan

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*===== typed js =====*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'UI/UX Designer', 'Freelancer', 'Photographer'],
+    strings: ['Web Developer', 'UI/UX Designer', 'Desainer Grafis', 'Video Editor'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
